84b88635c89d2991c27162c4e458cbf52e98789a
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2022-04-08 09:48:33 +0200

    Use shared Random

--- src/PizzaLight/Resources/ExtensionClasses/PersonExtensions.cs
+++ src/PizzaLight/Resources/ExtensionClasses/PersonExtensions.cs
@@ -11,14 +11,13 @@ namespace PizzaLight.Resources.ExtensionClasses
     {
         public static List<Person> SelectListOfRandomPeople(this List<User> inviteCandidates, int targetGuestCount)
         {
-            var random = new Random();
             var numberOfCandidates = inviteCandidates.Count;
             var numberToInvite = Math.Min(targetGuestCount, numberOfCandidates);
 
             var list = new List<Person>(numberToInvite);
             for (int i = 0; i < numberToInvite; i++)
             {
-                var randomPick = random.Next(0, numberOfCandidates);
+                var randomPick = Random.Shared.Next(0, numberOfCandidates);
                 var invite = inviteCandidates[randomPick];
                 inviteCandidates.Remove(invite);
                 numberOfCandidates--;
